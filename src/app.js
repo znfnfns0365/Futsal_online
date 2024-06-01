838a674df7f65bd1ce31439baa436c016d4ee23b
4cozm <17328rm@gmail.com>
2024-06-01 14:31:44 +0900

    add:파일디렉토리 설정

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended:true}));
+
+app.listen(PORT,()=>{
+    console.log("서버가 3001 포트에서 열렸습니다");
+});
