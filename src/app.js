a255a1929291b71584107b226228ab4976e8218c
znfnfns0365 <znfnfns0365@naver.com>
2024-06-02 18:22:11 +0900

    팀/감독 api

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 //import cookieParser from 'cookie-parser';
 //import errorHandlingMiddlewares from './middlewares/error-handling.middlewares.js';
 import accountRouter from './routers/accounts.js';
+import directorRouter from './routers/director.js';
 
 const app = express();
 const PORT = 3001;
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use('/api', [accountRouter]);
 app.use('/api/account', accountRouter);
+app.use('/api/director', directorRouter);
 
 app.listen(PORT, () => {
   console.log('서버가 3001 포트에서 열렸습니다');
--- src/routers/accounts.js
+++ src/routers/accounts.js
@@ -1,10 +1,10 @@
-import express from "express";
+import express from 'express';
 
-import { signIn,signUp } from "../controllers/accountControllers.js";
+import { signIn, signUp } from '../controllers/accountControllers.js';
 
 const accountRouter = express.Router();
 
-accountRouter.get('/signIn',signIn);
-accountRouter.post('/signUP',signUp);
+accountRouter.post('/signIn', signIn);
+accountRouter.post('/signUP', signUp);
 
 export default accountRouter;
