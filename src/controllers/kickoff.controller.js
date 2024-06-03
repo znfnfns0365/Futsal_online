ccfc6a70c757e2bf043980255930a26cca63c96a
znfnfns0365 <znfnfns0365@naver.com>
2024-06-03 20:19:08 +0900

    update 게임 로직 완성

--- src/controllers/kickoff.controller.js
+++ src/controllers/kickoff.controller.js
@@ -1,47 +1,8 @@
-import { userPrisma } from '../utils/prisma/index.js';
+import { userPrisma, matchPrisma } from '../utils/prisma/index.js';
+import { myPlayerInfo } from './player.controller.js';
 
 const Teams = userPrisma.teams;
-
-// delay용 함수
-function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-// 경기 진행 함수
-function gaming(team1, team2) {
-  //빈 포지션 있는지 확인
-  if (!(team1.fw && team1.mf && team1.df)) return 'team1';
-  else if (!(team2.fw && team2.mf && team2.df)) return 'team2';
-
-  const attackPointTeam1 = team1.fw * fwCondition1 + team1.mf * mfCondition1,
-    attackPointTeam2 = team2.fw * fwCondition2 + team2.mf * mfCondition2,
-    defensePointTeam1 = team1.df * dfCondition1 + team1.mf * mfCondition1,
-    defensePointTeam2 = team2.df * mfCondition2 + team2.mf * mfCondition2;
-  let team1Score = (function () {
-    const repeat = Math.floor((defensePointTeam2 - attackPointTeam1) / 10);
-    let score = 0;
-    for (let i = 0; i <= repeat; i++) {
-      if (Math().random % 10 < 5) {
-        // 골
-        score++;
-      }
-    }
-    return score;
-  })();
-  let team2Score = (function () {
-    const repeat = Math.floor((defensePointTeam1 - attackPointTeam2) / 10);
-    let score = 0;
-    // 수비 점수 - 공격 점수
-    for (let i = 0; i <= repeat; i++) {
-      if (Math.floor(Math.random() * 10) + 1 < 5) {
-        // 40% 확률로 득점 성공
-        score++;
-      }
-    }
-    return score;
-  })();
-  return [team1Score, team2Score];
-}
+const Matches = matchPrisma.matches;
 
 export const kickoff = async (req, res) => {
   const { director, opposingDirector } = req.params;
@@ -75,7 +36,7 @@ export const kickoff = async (req, res) => {
       .json({ errorMessage: '본인의 감독끼리는 게임할 수 없습니다.' });
   }
 
-  const result = gaming(myTeam.squad, opposingTeam.squad);
+  const result = gaming(myTeam, opposingTeam);
   if (result === 'team1') {
     return res
       .status(400)
@@ -85,10 +46,30 @@ export const kickoff = async (req, res) => {
       .status(400)
       .json({ errorMessage: '상대의 squad에 빈 포지션이 존재합니다' });
   }
+
+  // 경기 가능
   console.log('경기중!');
   await delay(2000);
 
-  return res.status(200).json({ myTeam, opposingTeam, result });
+  // 매치 정보, 결과 업데이트  함수 호출
+  await updateRecords(myTeam, opposingTeam, result);
+
+  // 무승부시 결과 출력
+  if (result[0] === result[1])
+    return res.status(200).json({
+      message: `${myTeam.name} vs ${opposingTeam.name}`,
+      result: `${result[0]} : ${result[1]}로 무승부`,
+      myRecord: `${myTeam.win}승 ${myTeam.draw}무 ${myTeam.lose}패`,
+    });
+
+  // 무승부가 아닐 경우 결과 출력
+  return res.status(200).json({
+    message: `${myTeam.name} vs ${opposingTeam.name}`,
+    result: `${result[0]} : ${result[1]}로 ${
+      result[0] > result[1] ? '승리!' : '패배..'
+    }`,
+    myRecord: `${myTeam.win}승 ${myTeam.draw}무 ${myTeam.lose}패`,
+  });
 };
 
 export const automaticKickoff = async (req, res) => {
@@ -109,7 +90,7 @@ export const automaticKickoff = async (req, res) => {
 
   const opposingTeam = 0; // 자동매치메이킹함수(myTeam)
 
-  const result = gaming(myTeam.squad, opposingTeam.squad);
+  const result = gaming(myTeam, opposingTeam);
 
   // 빈 포지션이 있을 때 (경기 불가)
   if (result === 'team1') {
@@ -125,5 +106,146 @@ export const automaticKickoff = async (req, res) => {
   // 경기 가능
   console.log('경기중!');
   await delay(2000);
-  return res.status(200).json({ myTeam, opposingTeam, result });
+  // 매치 정보, 결과 업데이트  함수 호출
+  await updateRecords(myTeam, opposingTeam, result);
+
+  // 무승부시 결과 출력
+  if (result[0] === result[1])
+    return res.status(200).json({
+      message: `${myTeam.name} vs ${opposingTeam.name}`,
+      result: `${result[0]} : ${result[1]}로 무승부`,
+      myRecord: `${myTeam.win}승 ${myTeam.draw}무 ${myTeam.lose}패`,
+    });
+
+  // 무승부가 아닐 경우 결과 출력
+  return res.status(200).json({
+    message: `${myTeam.name} vs ${opposingTeam.name}`,
+    result: `${result[0]} : ${result[1]}로 ${
+      result[0] > result[1] ? '승리!' : '패배..'
+    }`,
+    myRecord: `${myTeam.win}승 ${myTeam.draw}무 ${myTeam.lose}패`,
+  });
 };
+
+// delay용 함수
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// 경기 진행 함수
+function gaming(myTeam, opposingTeam) {
+  const team1 = myTeam.squad;
+  const team2 = opposingTeam.squad;
+
+  //빈 포지션 있는지 확인
+  if (!(team1.fw && team1.mf && team1.df)) return 'team1';
+  else if (!(team2.fw && team2.mf && team2.df)) return 'team2';
+
+  //컨디션 불러오기
+  //   const fwCondition1,
+  //     mfCondition1,
+  //     dfCondition1,
+  //     fwCondition2,
+  //     mfCondition2,
+  //     dfCondition2;
+
+  // 점수 로직
+  const attackPointTeam1 = team1.fw * fwCondition1 + team1.mf * mfCondition1,
+    defensePointTeam1 = team1.df * dfCondition1 + team1.mf * mfCondition1,
+    attackPointTeam2 = team2.fw * fwCondition2 + team2.mf * mfCondition2,
+    defensePointTeam2 = team2.df * dfCondition2 + team2.mf * mfCondition2;
+  let team1Score = (function () {
+    const repeat = Math.floor((defensePointTeam2 - attackPointTeam1) / 10);
+    let score = 0;
+    for (let i = 0; i <= repeat; i++) {
+      if (Math.floor(Math.random() * 10) + 1 < 5) {
+        // 40% 확률로 득점 성공
+        score++;
+      }
+    }
+    return score;
+  })();
+  let team2Score = (function () {
+    const repeat = Math.floor((defensePointTeam1 - attackPointTeam2) / 10);
+    let score = 0;
+    // 수비 점수 - 공격 점수
+    for (let i = 0; i <= repeat; i++) {
+      if (Math.floor(Math.random() * 10) + 1 < 5) {
+        // 40% 확률로 득점 성공
+        score++;
+      }
+    }
+    return score;
+  })();
+  return [team1Score, team2Score];
+}
+
+// 매치 정보, 결과 업데이트 함수
+async function updateRecords(myTeam, opposingTeam, result) {
+  // 매치 정보 입력
+  await Matches.create({
+    data: {
+      player1_id: myTeam.director,
+      player2_id: opposingTeam.director,
+      score_player1: result[0],
+      score_player2: result[1],
+    },
+  });
+  // 승리시 db 추가
+  if (result[0] > result[1]) {
+    await Teams.update({
+      data: {
+        win: myTeam.win + 1,
+      },
+      where: {
+        director: myTeam.director,
+      },
+    });
+    await Teams.update({
+      data: {
+        lose: opposingTeam.lose + 1,
+      },
+      where: {
+        director: opposingTeam.director,
+      },
+    });
+  }
+  // 패배시 db 추가
+  else if (result[0] < result[1]) {
+    await Teams.update({
+      data: {
+        win: opposingTeam.win + 1,
+      },
+      where: {
+        director: opposingTeam.director,
+      },
+    });
+    await Teams.update({
+      data: {
+        lose: myTeam.lose + 1,
+      },
+      where: {
+        director: myTeam.director,
+      },
+    });
+  }
+  // 무승부시 db 추가 및 출력
+  else {
+    await Teams.update({
+      data: {
+        draw: opposingTeam.draw + 1,
+      },
+      where: {
+        director: opposingTeam.director,
+      },
+    });
+    await Teams.update({
+      data: {
+        draw: myTeam.draw + 1,
+      },
+      where: {
+        director: myTeam.director,
+      },
+    });
+  }
+}
