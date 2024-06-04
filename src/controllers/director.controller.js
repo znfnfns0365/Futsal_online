0181b113f777f881e066b52feedf1c2520ed1bb7
4cozm <17328rm@gmail.com>
2024-06-05 01:14:27 +0900

    add,fix: 로그인 미들웨어 수정,감독 생성시 자동뽑기 로직추가
    미들웨어 수정은 return 할 데이터에 bearer token 정보를 같이 넘겨주기 위함

--- src/controllers/director.controller.js
+++ src/controllers/director.controller.js
@@ -1,63 +1,88 @@
 import jwt from 'jsonwebtoken';
-import express from 'express';
+import express, { json } from 'express';
 import dotenv from 'dotenv';
 import Joi from 'joi';
 import bcrypt from 'bcrypt';
-import { userPrisma,playerPrisma } from '../utils/prisma/index.js';
+import { userPrisma, playerPrisma } from '../utils/prisma/index.js';
+import { gacha } from './player.controller.js'; // 감독 생성시 선수 3명을 자동 지급하기 위해서
 
 const Teams = userPrisma.teams;
 const Budget = userPrisma.budget;
-const positionVali = ["df", "fw", "mf"];
+const positionVali = ['df', 'fw', 'mf'];
 
 /* 감독 생성 API */
 export const createDirector = async (req, res) => {
-  const { director, name } = req.body;
-  const user = req.user;
+  try {
+    const { director, name } = req.body;
+    const user = req.user;
 
-  //동일한 director 이름 검사
-  const sameName = await Teams.findFirst({
-    // 동일한 director가 User에게 있는지 찾음
-    where: { director, User_Id: user.user_Id },
-  });
-  if (sameName) {
-    // 있다면 에러 메시지 전송
-    return res
-      .status(400)
-      .json({ errorMessage: '동일한 이름을 가진 감독이 존재합니다' });
-  }
-
-  //동일한 팀 이름(name) 검사
-  const sameTeam = await Teams.findFirst({
-    // 동일한 director가 User에게 있는지 찾음
-    where: { name, User_Id: user.user_Id },
-  });
-  if (sameTeam) {
-    // 있다면 에러 메시지 전송
-    return res
-      .status(400)
-      .json({ errorMessage: '동일한 이름을 가진 팀이 존재합니다' });
-  }
-
-  const newTeam = await Teams.create({
-    // 팀 추가
-    data: {
-      director,
-      User_id: user.user_id,
-      name,
-      candidate_players: {},
-      squad: {
-        fw: false,
-        mf: false,
-        df: false,
-      },
-    },
-  });
-  await Budget.create({
-    data: {
-      Director: newTeam.director
+    //동일한 director 이름 검사
+    const sameName = await Teams.findFirst({
+      // 동일한 director가 User에게 있는지 찾음
+      where: { director, User_Id: user.user_Id },
+    });
+    if (sameName) {
+      // 있다면 에러 메시지 전송
+      return res
+        .status(400)
+        .json({ errorMessage: '동일한 이름을 가진 감독이 존재합니다' });
     }
-  })
-  return res.status(201).json({ newTeam });
+
+    //동일한 팀 이름(name) 검사
+    const sameTeam = await Teams.findFirst({
+      // 동일한 director가 User에게 있는지 찾음
+      where: { name, User_Id: user.user_Id },
+    });
+    if (sameTeam) {
+      // 있다면 에러 메시지 전송
+      return res
+        .status(400)
+        .json({ errorMessage: '동일한 이름을 가진 팀이 존재합니다' });
+    }
+
+    const newTeam = await Teams.create({
+      // 팀 추가
+      data: {
+        director,
+        User_id: user.user_id,
+        name,
+        candidate_players: {},
+        squad: {
+          fw: false,
+          mf: false,
+          df: false,
+        },
+      },
+    });
+    await Budget.create({
+      data: {
+        Director: newTeam.director,
+      },
+    });
+    //3명의 새 선수 지급 다만 무료로 주는건 아니고 자동으로 뽑음...
+    const freeGacha =[];
+    for (let i = 0; i < 3; i++) {
+      console.log(i+"번째 실행중@@@@@@");
+      await fetch(`http://localhost:3001/api/store/gacha/${director}`, {
+        //URL은 추후 변경할 예정
+        method: 'POST',
+        headers: {
+          'Authorization': req.token, //토큰값을 넣어줘야함
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          freeGacha.push(data);
+        })
+        .catch((error) => {
+          console.log('Error:', error);
+        });
+    }
+
+    return res.status(201).json({ newTeam,...freeGacha });
+  } catch (error) {
+    return res.status(500).json({ errorMessage: error.message });
+  }
 };
 
 /* 감독 조회 API */
@@ -178,8 +203,9 @@ export const updateDirector = async (req, res) => {
   });
 
   return res.status(200).json({
-    message: `팀 ${newName ?? name}, ${newDirector ?? director
-      } 감독으로 수정되었습니다.`,
+    message: `팀 ${newName ?? name}, ${
+      newDirector ?? director
+    } 감독으로 수정되었습니다.`,
   });
 };
 
@@ -249,13 +275,11 @@ export const checkDirectorTeam = async (req, res) => {
     return res
       .status(404)
       .json({ errorMessage: `df에 선수를 등록하지 않았습니다.` });
-  }
-  else if (!fw) {
+  } else if (!fw) {
     return res
       .status(404)
       .json({ errorMessage: `fw에 선수를 등록하지 않았습니다.` });
-  }
-  else if (!mf) {
+  } else if (!mf) {
     return res
       .status(404)
       .json({ errorMessage: `mf에 선수를 등록하지 않았습니다.` });
@@ -269,7 +293,7 @@ export const changeTeamPlayer = async (req, res) => {
   try {
     const director = req.params.director; // parameter 가져오기
     const { id } = req.body; // req.body로 position과 id 가져오기
-    let { position } =  req.body;
+    let { position } = req.body;
     const user = req.user;
 
     // 포지션의 대문자 값을 소문자로 변경
@@ -305,29 +329,31 @@ export const changeTeamPlayer = async (req, res) => {
     });
 
     // 해당 감독의 인벤토리에 body으로 받은 id 를 가진 player들중 제일 앞 요소를 가져옴
-    const isExistPlayer = CandidatePlayers.candidate_players.find(player => player.id === id);
+    const isExistPlayer = CandidatePlayers.candidate_players.find(
+      (player) => player.id === id
+    );
 
     // 해당 감독의 인벤토리에 없다면 404 code를 return
     if (!isExistPlayer) {
-      return res
-        .status(404)
-        .json({ errorMessage: `${director} 감독은 ${id} 라는 id를 가진 선수를 소지하고 있지 않습니다.` });
+      return res.status(404).json({
+        errorMessage: `${director} 감독은 ${id} 라는 id를 가진 선수를 소지하고 있지 않습니다.`,
+      });
     }
 
     // 포지션의 입력이 제대로 되었는가 확인
     if (!positionVali.includes(position)) {
-      return res
-        .status(404)
-        .json({ errorMessage: `잘못된 포지션을 입력하셨습니다. 입력하신 포지션 = ${position}` });
+      return res.status(404).json({
+        errorMessage: `잘못된 포지션을 입력하셨습니다. 입력하신 포지션 = ${position}`,
+      });
     }
 
     const squad = team.squad;
     //선수의 player_unique_id,id,condition 정보가 들어가야 한다
     const squadPlayer = {
-      "player_unique_id": isExistPlayer.player_unique_id,
-      "id": isExistPlayer.id,
-      "condition": isExistPlayer.condition,
-    }
+      player_unique_id: isExistPlayer.player_unique_id,
+      id: isExistPlayer.id,
+      condition: isExistPlayer.condition,
+    };
 
     //트랜잭션을 시작 (스쿼드에 선수가 이미 있는 경우 선수를 빼주고 다시 넣어야하니까!)
 
@@ -337,18 +363,25 @@ export const changeTeamPlayer = async (req, res) => {
       //선수가 있는경우 -> 그 선수를 다시 락커룸으로 보내고 새로운 선수를 할당
       if (squad[position]) {
         CandidatePlayers.candidate_players.push(squad[position]); // 스쿼드에 있는 선수 -> 락커룸
-        squad[position] = isExistPlayer;                          // 락커룸에 있던 지정한 선수 -> 스쿼드
+        squad[position] = isExistPlayer; // 락커룸에 있던 지정한 선수 -> 스쿼드
         // 스쿼드로 들어간 선수는 락커룸에서 제외
-        CandidatePlayers.candidate_players = CandidatePlayers.candidate_players.filter(player => player.id !== id);
-        console.log(JSON.stringify(CandidatePlayers.candidate_players, null, 2));
+        CandidatePlayers.candidate_players =
+          CandidatePlayers.candidate_players.filter(
+            (player) => player.id !== id
+          );
+        console.log(
+          JSON.stringify(CandidatePlayers.candidate_players, null, 2)
+        );
         console.log(JSON.stringify(squad, null, 2));
-      }
-      else  //선수가 없는경우 -> 그냥 새로운 선수를 할당
-      {
+      } //선수가 없는경우 -> 그냥 새로운 선수를 할당
+      else {
         // 비어있는 스쿼드에 락커룸의 선수를 출전시킴
         squad[position] = isExistPlayer;
         // 출전 시킨 선수는 락커룸에서 제외
-        CandidatePlayers.candidate_players = CandidatePlayers.candidate_players.filter(player => player.id !== id);
+        CandidatePlayers.candidate_players =
+          CandidatePlayers.candidate_players.filter(
+            (player) => player.id !== id
+          );
       }
 
       // 바꾼 정보를 DB에 다시 입력
@@ -356,49 +389,46 @@ export const changeTeamPlayer = async (req, res) => {
         where: {
           director: director,
         },
-        data :{
-          candidate_players : CandidatePlayers.candidate_players,
-          squad : squad,
-        }
-      })
-
-    })
+        data: {
+          candidate_players: CandidatePlayers.candidate_players,
+          squad: squad,
+        },
+      });
+    });
     return res.status(200).json(team.squad);
-  }
-  catch (error) {
+  } catch (error) {
     res.status(500).json({ errorMessage: error.message });
   }
 };
 
-
 //rating 순서대로 감독을 줄세워야함
 
-export const ranking = async(req,res)=>{
+export const ranking = async (req, res) => {
   //모든 팀을 불러와서 감독명,팀명,레이팅,승,무,패 정보를 가져온다
-try{
-  const result = await Teams.findMany({
-    where:{
-      OR:[
-        {win:{gt:0}},
-        {draw:{gt:0}},
-        {lose:{gt:0}}
-    ]},
-    orderBy:{
-      rating:"asc"
+  try {
+    const result = await Teams.findMany({
+      where: {
+        OR: [{ win: { gt: 0 } }, { draw: { gt: 0 } }, { lose: { gt: 0 } }],
+      },
+      orderBy: {
+        rating: 'asc',
+      },
+      select: {
+        director: true,
+        name: true,
+        rating: true,
+        win: true,
+        draw: true,
+        lose: true,
+      },
+    });
+    if (!result) {
+      return res.status(404).json({ message: '경기 내역이 존재하지 않습니다' });
     }
-    ,
-    select:{
-      director:true,
-      name:true,
-      rating:true,
-      win:true,
-      draw:true,
-      lose:true
-    }
-  })
-  if(!result){return res.status(404).json({message:"경기 내역이 존재하지 않습니다"})}
-  return res.status(200).json({result});
-}catch(error){
-  return res.status(500).json({errorMessage:error.message+"rating 기능에서 오류 발생"})
-}
-}
+    return res.status(200).json({ result });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ errorMessage: error.message + 'rating 기능에서 오류 발생' });
+  }
+};
