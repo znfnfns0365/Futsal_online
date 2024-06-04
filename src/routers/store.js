9b72bc3e4db1352e58f51870b9520cf034f92359
znfnfns0365 <znfnfns0365@naver.com>
2024-06-04 16:58:24 +0900

    add kickoff.js, kickoff.controller.js

--- src/routers/store.js
+++ src/routers/store.js
@@ -1,11 +1,9 @@
 import express from 'express';
 import authMiddleware from '../middlewares/auth.middleware.js';
 import { gacha } from '../controllers/player.controller.js';
-
 const storeRouter = express.Router();
 
 // /api/store/ 에 대한 라우터들을 정의
-storeRouter.post('/gacha/:director', authMiddleware, gacha);
 
 storeRouter.post('/gacha/:director', authMiddleware, gacha);
 
