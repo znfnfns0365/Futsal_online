0c54c044e0572af2178866eccca6380488e61b70
moonhyunhu <ansgusgn@gmaile.com>
2024-06-05 10:26:35 +0900

    fix:코드수정

---
+++ src/controllers/match.cndtroller.js
@@ -0,0 +1,10 @@
+import { userPrisma, playerPrisma } from '../utils/prisma/index.js';
+
+//전적 조회 API
+export const recordsCheck = async (req, res, next) => {
+    //경로 매개변수로 조회할 팀명을 받아온다
+    const {userId} = req.params;
+
+    //조회할 감독ㅓㄹ
+  };
+  
---
+++ src/routers/matches.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import authMiddleware from '../middlewares/auth.middleware.js';
+
+const matchRouter = express.Router();
+
+matchRouter.get('/:director', );
+
+
+export default matchRouter;
