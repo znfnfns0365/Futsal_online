1f95afbd70452316d31c3e0ee8c9a9323aa60ea7
moonhyunhu <ansgusgn@gmaile.com>
2024-06-06 18:03:04 +0900

    update:선수구매 이적시 매물 조회 기능 추가

--- src/routers/store.js
+++ src/routers/store.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import authMiddleware from '../middlewares/auth.middleware.js';
 import { gacha } from '../controllers/player.controller.js';
-import { sellPlayer } from '../controllers/store.controller.js';
+import { sellPlayer, storeList, buyPlayer } from '../controllers/store.controller.js';
 const storeRouter = express.Router();
 
 // /api/store/ 에 대한 라우터들을 정의
 
 storeRouter.post('/gacha/:director', authMiddleware, gacha);
-storeRouter.post('/sell',authMiddleware,sellPlayer);
+storeRouter.post('/sell', authMiddleware, sellPlayer);
+storeRouter.get('/', storeList);
+storeRouter.patch('/buy', authMiddleware, buyPlayer);
 export default storeRouter;
